1dc4afc888deda858ebff40bdb21a01d9211e379
femilande <lande.enitan@gmail.com>
2019-05-17 17:48:49 -0400

    UbuntuDevOpsBox

---
+++ README.md
@@ -0,0 +1,16 @@
+# Terraform-Devops-Box
+# DevOps box
+* A vagrant project with an ubuntu box with the tools needed to do DevOps
+
+# tools included
+* Terraform
+* AWS CLI
+* Ansible
+* Packer
+* Docker
+* Python3X
+* Kubectl
+* Python 2.7
+
+
+# This scripts need more improvement. You can contribute to make it more efficient.
---
+++ scripts/install.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+set -x
+
+TERRAFORM_VERSION="0.11.11"
+PACKER_VERSION="0.10.2"
+# create new ssh key
+[[ ! -f /home/ubuntu/.ssh/mykey ]] \
+&& mkdir -p /home/ubuntu/.ssh \
+&& ssh-keygen -f /home/ubuntu/.ssh/mykey -N '' \
+&& chown -R ubuntu:ubuntu /home/ubuntu/.ssh
+
+# install packages
+apt-get update
+apt-get -y install docker.io ansible unzip
+# add docker privileges
+usermod -G docker ubuntu
+# install pip
+pip install -U pip && pip3 install -U pip
+if [[ $? == 127 ]]; then
+    wget -q https://bootstrap.pypa.io/get-pip.py
+    python get-pip.py
+    python3 get-pip.py
+fi
+# install awscli and ebcli
+pip install -U awscli
+pip install -U awsebcli
+
+#terraform
+T_VERSION=$(terraform -v | head -1 | cut -d ' ' -f 2 | tail -c +2)
+T_RETVAL=${PIPESTATUS[0]}
+
+[[ $T_VERSION != $TERRAFORM_VERSION ]] || [[ $T_RETVAL != 0 ]] \
+&& wget -q https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip \
+&& unzip -o terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /usr/local/bin \
+&& rm terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+
+# packer
+P_VERSION=$(packer -v)
+P_RETVAL=$?
+
+[[ $P_VERSION != $PACKER_VERSION ]] || [[ $P_RETVAL != 1 ]] \
+&& wget -q https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip \
+&& unzip -o packer_${PACKER_VERSION}_linux_amd64.zip -d /usr/local/bin \
+&& rm packer_${PACKER_VERSION}_linux_amd64.zip
+
+#kuctl
+sudo apt-get update && sudo apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install software-properties-common
+sudo apt-get install -y kubectl
+
+# Install net-tools
+sudo apt-get install net-tools
+sudo apt-get install nmap
+
+# Install Scanning & Monitoring Tools
+sudo apt-get install lynis -y
+sudo apt-get install iotop -y
+sudo apt-get install htop*
+sudo apt-get install sysstat -y
+
+# Color
+sudo apt-get install ccze
+sudo add-apt-repository ppa:dawidd0811/neofetch
+sudo apt-get update
+sudo apt-get install neofetch
+sudo apt-get install sysvbanner
+
+# clean up
+apt-get clean
